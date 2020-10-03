c142c74df755684e779982f9071ade2cb6cb991c
hacklover <info@hacklover.net>
2020-10-03 10:29:18 +0200

    Update libraries

--- vue.config.js
+++ vue.config.js
@@ -17,7 +17,7 @@ module.exports = {
   css: {
     loaderOptions: {
       scss: {
-        prependData: '@import "~/assets/css/variables.scss";'
+        additionalData: '@import "~/assets/css/variables.scss";'
       }
     }
   },

b28b9404180860db4177b8b7a039ac6beaf9c8b2
hacklover <info@hacklover.net>
2020-10-20 23:26:26 +0200

    Remove generic events from App.vue (moved to core Desktop.vue component)

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -5,17 +5,12 @@
 </template>
 
 <script>
-import mixinServer from '@/core/mixins/mixinServer'
-
 export default {
   name: 'App',
-  mixins: [mixinServer],
   computed: {
     /**
-       * Manage classes on #app element
-       *
-       * @returns {[]}
-       */
+     * Manage classes on #app element
+     */
     appClasses() {
       const appClasses = []
 
@@ -38,48 +33,6 @@ export default {
 
       return appClasses
     }
-  },
-  beforeMount() {
-    const self = this
-
-    // redirect to homepage on 404
-    if (!this.$route.name) {
-      this.$router.push({ name: 'index' })
-    }
-
-    // on page ready, connect to SSE
-    if (this.isServerAvailable) {
-      window.addEventListener('load', function() {
-        self.$store.dispatch('core/sse/connect', 'once')
-      })
-    }
-  },
-  created() {
-    const self = this
-
-    // initialize client
-    this.$store.dispatch('core/client/initialize')
-
-    // add window resize event
-    window.addEventListener('resize', function () {
-      clearTimeout(this.handlePageResize)
-
-      this.handlePageResize = setTimeout(() => {
-        self.$store.commit('core/windows/SET_DESKTOP_WIDTH', window.innerWidth)
-        self.$store.commit('core/windows/SET_DESKTOP_HEIGHT', window.innerHeight)
-
-        // windows position
-        self.$store.dispatch('core/windows/windowsHandlePageResize')
-      }, 100)
-    })
-  },
-  destroyed() {
-    const self = this
-
-    // remove window resize event
-    window.removeEventListener('resize', function () {
-      self.$store.dispatch('core/windows/windowsHandlePageResize')
-    })
   }
 }
 </script>

c954f8978982d1ef9b3ffcac07596c40e33f31d3
hacklover <info@hacklover.net>
2020-10-22 20:47:49 +0200

    Improve routes handling

--- packages/client/client.config.js
+++ packages/client/client.config.js
@@ -1,17 +1,13 @@
 // import all pages routes
 import routesMain from '@/pages/main/routes'
 
-// merge all pages routes into an unique pages array
-const pages = [].concat(
-  routesMain
-)
-
 // modules configuration
-const modulesConfig = require(`@/../config/modules.json`)
+const modulesConfig = require('@/../config/modules.json')
 
 export default {
-  pages,
-  plugins: [],
+  routes: [
+    routesMain
+  ],
   modules: modulesConfig,
 
   // vuetify config

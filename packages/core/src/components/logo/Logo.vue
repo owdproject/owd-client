e19f71d52dfc774c269f1dca001952240ca05e05
hacklover <info@hacklover.net>
2021-01-28 03:13:58 +0100

    Remove vue-typer dependency from core

--- packages/core/src/components/logo/Logo.vue
+++ packages/core/src/components/logo/Logo.vue
@@ -1,28 +1,12 @@
 <template>
   <div id="logo">
     <h2 v-text="title" />
-    <!--
-    <h2>
-      <vue-typer
-        :text="title"
-        shuffle
-        :repeat="0"
-        :type-delay="50"
-        erase-style="select-back"
-      />
-    </h2>
-    -->
   </div>
 </template>
 
 <script>
-// import {VueTyper} from 'vue-typer'
-
 export default {
   name: 'Logo',
-  components: {
-    // VueTyper
-  },
   props: {
     title: {
       type: String,

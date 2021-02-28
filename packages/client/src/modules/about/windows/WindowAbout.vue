cdd408263e4d2d79673d9bf472dbbde435f6c990
hacklover <info@hacklover.net>
2021-02-28 15:05:53 +0100

    Rename sample module to about module

---
+++ packages/client/src/modules/about/windows/WindowAbout.vue
@@ -0,0 +1,40 @@
+<template>
+  <Window :title="data.title" :window="data">
+    <div class="container">
+      <h2>
+        Open Web Desktop <span v-text="$store.state.core.client.version" />
+      </h2>
+      <a href="https://github.com/owdproject/owd-client">github.com/owdproject/owd-client</a>
+    </div>
+  </Window>
+</template>
+
+<script>
+  import Window from '@owd-client/core/src/components/window/Window'
+
+  export default {
+    components: {Window},
+    props: {
+      data: Object
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  div.container {
+    text-align: center;
+    margin-top: 50px;
+
+    h2 {
+      margin-bottom: 6px;
+
+      span {
+        opacity: 0.65;
+      }
+    }
+
+    a {
+      color: #5198ca;
+    }
+  }
+</style>

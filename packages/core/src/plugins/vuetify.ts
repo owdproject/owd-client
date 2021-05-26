a677426ece0b5554681c0109e93ce63d0c8b8152
hacklover <info@hacklover.net>
2021-05-26 14:31:19 +0200

    Back to vuetify 3.0.0-alpha.4 but restore old vuetify.min.css import location

--- packages/core/src/plugins/vuetify.ts
+++ packages/core/src/plugins/vuetify.ts
@@ -2,8 +2,6 @@
 import { createVuetify } from 'vuetify/dist/vuetify.js'
 import {App} from "@owd-client/types";
 
-import 'vuetify/dist/vuetify.min.css';
-
 export const owdCreateVuetify = function(app: App) {
   return createVuetify(app.config.owd.vuetify)
 }

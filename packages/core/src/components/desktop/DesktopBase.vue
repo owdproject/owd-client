e5403af2e9fd8e49861f641fff31773e81e5cd1f
hacklover <info@hacklover.net>
2021-06-16 08:42:56 +0200

    Rename Desktop.vue to DesktopBase.vue and use it in individual desktops (linux, windows)

---
+++ packages/core/src/components/desktop/DesktopBase.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="owd-desktop">
+    <slot />
+  </div>
+</template>
+
+<script setup>
+import { getCurrentInstance, onMounted, onUnmounted } from 'vue'
+import {useStore} from "vuex";
+
+const app = getCurrentInstance()
+const store = useStore()
+
+// initialize client
+onMounted(() => store.dispatch('core/client/initialize'))
+
+// handle desktop resize event
+onMounted(() => window.addEventListener('resize', handleDesktopResize))
+onUnmounted(() => window.removeEventListener('resize', handleDesktopResize))
+
+let timeoutHandleDesktopResize = null
+
+function handleDesktopResize() {
+  clearTimeout(timeoutHandleDesktopResize)
+
+  timeoutHandleDesktopResize = setTimeout(() => {
+    store.dispatch('core/window/windowsAdjustPosition')
+  }, 100)
+}
+</script>
+
+<style lang="scss">
+.owd-desktop {
+  display: flex;
+  flex-flow: column;
+  height: 100vh;
+
+  &__content {
+    position: relative;
+    overflow: hidden;
+    flex: 1;
+  }
+}
+</style>

ef73f3fbb77148ef1e4439cf38221b571275718f
hacklover <info@hacklover.net>
2021-10-18 19:57:15 +0200

    Re-add timeout workaround on desktopApps init

--- packages/core/src/libraries/core/modules/app/desktopApps.ts
+++ packages/core/src/libraries/core/modules/app/desktopApps.ts
@@ -11,7 +11,7 @@ export default class DesktopApps {
     this.context = context
 
     // on desktop components ready
-    this.initialize()
+    setTimeout(() => this.initialize(), 1)
   }
 
   /**
